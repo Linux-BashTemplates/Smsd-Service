4b7a49e36c4d25ca3a4d15ce37e525fb4833ed3c
Sergey Zhyltsov <48886027+SergRikhter@users.noreply.github.com>
2023-10-14 22:53:00 +0300

    Update and rename sms_smshandler.sh to sms.sh

---
+++ sms.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+log_dir="/var/log/sms"
+
+if [ ! -d "$log_dir" ]; then
+    mkdir -p "$log_dir"
+fi
+
+event_type="$1"
+input_file="$2"
+
+function runHandler() {
+    case "$1" in
+        RECEIVED)
+            timestamp=$(date +"%Y%m%d%H%M%S") # Получить текущую дату и время без разделителей
+            log_file="$log_dir/received_$timestamp.txt" # Уникальное имя файла на основе времени
+            echo "Timestamp: $timestamp" >> "$log_file"
+            head -5 "$input_file" | grep -e "^From: " -e "^Received: " >> "$log_file"
+            if grep "Alphabet: UCS2" "$input_file" > /dev/null; then
+                sed -e '1,/^$/ d' "$input_file" | recode UCS-2..utf8 >> "$log_file"
+            else
+                sed -e '1,/^$/ d' "$input_file" >> "$log_file"
+            fi
+            echo "========================================" >> "$log_file"
+            ;;
+        *)
+            echo "Unknown event type: $event_type"
+            exit 1
+            ;;
+    esac
+}
+
+function Run() {
+    if [ $# -lt 2 ]; then
+        echo "Usage: $0 <event_type> <input_file>"
+        exit 1
+    fi
+    runHandler "$1" "$2" 
+}
+
+Run "$event_type" "$input_file" 
